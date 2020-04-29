5a5d9844f085d1a64a982278dc95573a307a3b48
Bardarz <aspha9@outlook.com>
2020-04-29 20:38:14 +0200

    add venv script

---
+++ scripts/venvinstall.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+set -e
+
+# Ensure this script is called from the correct folder
+if [ ! -d scripts ]; then
+    echo "This script needs to be called from the root folder, i.e. ./scripts/venvinstall.sh"
+    exit 1
+fi
+
+if [ ! -d venv ]; then
+    # Create virtual environment
+    echo "Creating python venv"
+    python3 -m venv venv
+fi
+
+# Upgrade pip
+./venv/bin/pip install pip --upgrade
+
+# Install requirements.txt
+./venv/bin/pip install -r requirements.txt
