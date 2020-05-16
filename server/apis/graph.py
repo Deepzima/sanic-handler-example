36a7241cc476ae318b51bc5a0846d283e2687ca2
bardarz <aspha9@outlook.com>
2020-05-16 11:49:58 +0200

    New project structure

--- server/apis/graph.py
+++ server/apis/graph.py
@@ -1,15 +1,14 @@
 from sanic import Blueprint
 from sanic_graphql import GraphQLView
-from server.resolvers import schema_test
-from server.resolvers.user.user import user_schema
+# from server.resolvers import schema_test
+# from server.resolvers.user.user import user_schema
+
+from server.apis.grapql.resolvers.user import user_schema
 
 
 bp = Blueprint('graph', url_prefix='/graph')
 
 
-bp.add_route(GraphQLView.as_view(schema=schema_test, graphiql=True), '/graphql/test')
-bp.add_route(GraphQLView.as_view(schema=schema_test, batch=True), '/graphql/test/batch')
-
-bp.add_route(GraphQLView.as_view(schema=schema_test, graphiql=True), '/graphql/user')
-bp.add_route(GraphQLView.as_view(schema=schema_test, batch=True), '/graphql/user/batch')
+bp.add_route(GraphQLView.as_view(schema=user_schema, graphiql=True), '/graphql/user')
+bp.add_route(GraphQLView.as_view(schema=user_schema, batch=True), '/graphql/user/batch')
 
--- server/apis/grapql/resolvers/user.py
+++ server/apis/grapql/resolvers/user.py
@@ -5,10 +5,10 @@ from graphene import ObjectType, Schema, String
 
 
 class Query(ObjectType):
-    hello = String(description="Hello Zima?")
+    hello = String(description="")
 
     def resolve_hello(self, info):
         return "World"
 
 
-schema_test = Schema(query=Query)
+user_schema = Schema(query=Query)
